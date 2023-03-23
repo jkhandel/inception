780272058350d85cc8818e816190d5c1fd6f3d78
Juhi Khandelwal <juhi_khandelwal@persistent.com>
2023-03-23 14:51:43 +0530

    episode -1

---
+++ App.js
@@ -0,0 +1,5 @@
+
+      const heading = React.createElement("h1",{id:"klkl"}, [React.createElement("h1",{id:"klkl"},"kjj"),React.createElement("h1",{id:"klkl"},
+      "ghfghfg")])
+     const root = React.createRoot(document.getElementById("root"));
+     root.render(heading);
